a537ab5459c8db5bca4e0b37e6216c05e49f6bd1
Matteo Olivi <molivi@anynines.com>
2021-12-09 16:22:12 +0100

    Fix fluentd image

--- update_images.sh
+++ update_images.sh
@@ -10,12 +10,12 @@ update_fluentd_img_and_commit () {
     local NEW_VERSION=$1
     local MANIFEST="deploy/logging/collection-infrastructure/fluentd-aggregator.yaml"
 
-    local GET_VERSION_SED_CMD="s/^[[:space:]]\{1,\}image:[[:space:]].\{1,\}\/fluentd:\(v[\.-[:digit:]]\{1,\}\)\"\{0,1\}$/\1/p"
+    local GET_VERSION_SED_CMD="s/^[[:space:]]\{1,\}image:[[:space:]].\{1,\}\/fluentd:\(v[\.[:digit:]-]\{1,\}\)\"\{0,1\}$/\1/p"
     local CURRENT_VERSION=$(gsed -n $GET_VERSION_SED_CMD $MANIFEST)
 
     if [[ "$NEW_VERSION" > "$CURRENT_VERSION" ]]
     then
-        local UPDATE_VERSION_SED_CMD="s/^\([[:space:]]\{1,\}image:[[:space:]].\{1,\}\/$COMPONENT:\)v[\.[:digit:]]\{1,\}\(\"\{0,1\}\)$/\1$NEW_VERSION\2/"
+        local UPDATE_VERSION_SED_CMD="s/^\([[:space:]]\{1,\}image:[[:space:]].\{1,\}\/fluentd:\)v[\.[:digit:]-]\{1,\}\(\"\{0,1\}\)$/\1$NEW_VERSION\2/"
         gsed -i "$UPDATE_VERSION_SED_CMD" "$MANIFEST"
         # TODO: Uncomment before pushing real version.
         echo "Bump fluentd-aggregator to $NEW_VERSION"
@@ -50,7 +50,7 @@ update_core_component_img_and_commit () {
 }
 
 main () {
-    local VERSIONED_IMGS="postgresql-operator:v0.9.0 backup-manager:v0.7.0 service-binding-controller:v0.5.0"
+    local VERSIONED_IMGS="postgresql-operator:v0.9.0 backup-manager:v0.7.0 service-binding-controller:v0.5.0 fluentd:v0.12.3-1.0-1.1.0"
     for VERSIONED_IMG in $VERSIONED_IMGS
     do
         # Extract image name and version as separate variables
