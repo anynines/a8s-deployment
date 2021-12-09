73986f9e53158240a616be83f9b9201a7d585954
Matteo Olivi <molivi@anynines.com>
2021-12-09 09:55:23 +0100

    [WIP] Keep adding logic to update images

--- update_images.sh
+++ update_images.sh
@@ -11,11 +11,13 @@ do
     # This regex could be stricter, but right now it's enough and it's much more readable this way
     # than in "stricter" versions.
     IMG_FIELD_REGEXP="s/^[[:space:]]\{1,\}image:[[:space:]].\{1,\}\/$IMG:v\([[:digit:]]\{1,\}.[[:digit:]]\{1,\}.[[:digit:]]\{1,\}\)\"\{0,1\}/\1/p"
-    CURRENT_VERSION=$(sed -n $IMG_FIELD_REGEXP "deploy/a8s/$IMG.yaml")
+    CURRENT_VERSION=$(gsed -n $IMG_FIELD_REGEXP "deploy/a8s/$IMG.yaml")
     NEW_VERSION="11.53.22"
     # TODO: Add comment on fragility of this check.
     if [[ "$NEW_VERSION" > "$CURRENT_VERSION" ]]
     then
+        UPDATE_IMG_FIELD_REGEXP="s/^\([[:space:]]\{1,\}image:[[:space:]].\{1,\}\/$IMG:v\)[[:digit:]]\{1,\}.[[:digit:]]\{1,\}.[[:digit:]]\{1,\}\(\"\{0,1\}\)/\1$NEW_VERSION\2/"
+        gsed -i $UPDATE_IMG_FIELD_REGEXP "deploy/a8s/$IMG.yaml"
         echo "Updated $IMG from v$CURRENT_VERSION to v$NEW_VERSION"
     else
         echo "$IMG current version in a8s is v$CURRENT_VERSION, most recent version found is v$NEW_VERSION, no update needed"
