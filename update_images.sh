7368934a4ff671dccc5f921ab51adcee83bf75d4
Matteo Olivi <molivi@anynines.com>
2021-12-09 15:38:48 +0100

    Refactor for readability

--- update_images.sh
+++ update_images.sh
@@ -6,6 +6,28 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Right now the core components are the PostgreSQL Operator, the Backup Manager and the Service
+# Binding Controller.
+update_core_component_img_and_commit () {
+    local component=$1
+    local new_version=$2
+    local manifest="deploy/a8s/$component.yaml"
+
+    local get_version_sed_cmd="s/^[[:space:]]\{1,\}image:[[:space:]].\{1,\}\/$component:\(v[\.[:digit:]]\{1,\}\)\"\{0,1\}$/\1/p"
+    local current_version=$(gsed -n $get_version_sed_cmd $manifest)
+
+    if [[ "$new_version" > "$current_version" ]]
+    then
+        local update_version_sed_cmd="s/^\([[:space:]]\{1,\}image:[[:space:]].\{1,\}\/$component:\)v[\.[:digit:]]\{1,\}\(\"\{0,1\}\)$/\1$new_version\2/"
+        gsed -i "$update_version_sed_cmd" "$manifest"
+        # TODO: Uncomment before pushing real version.
+        # git add "$manifest"
+        # git commit -m "Bump $img to $new_version"
+    else
+        echo "$component current version is $current_version, most recent version found is $new_version, no update needed"
+    fi
+}
+
 VERSIONED_IMGS="postgresql-operator:v0.9.0 backup-manager:v0.7.0 service-binding-controller:v0.5.0"
 for VERSIONED_IMG in $VERSIONED_IMGS
 do
@@ -13,24 +35,15 @@ do
     IMG=$(echo $VERSIONED_IMG | cut -d ':' -f 1)
     NEW_VERSION=$(echo $VERSIONED_IMG | cut -d ':' -f 2)
 
-    GET_IMG_FIELD_REGEXP="s/^[[:space:]]\{1,\}image:[[:space:]].\{1,\}\/$IMG:\(v[\.[:digit:]]\{1,\}\)\"\{0,1\}$/\1/p"
-    UPDATE_IMG_FIELD_REGEXP="s/^\([[:space:]]\{1,\}image:[[:space:]].\{1,\}\/$IMG:\)v[\.[:digit:]]\{1,\}\(\"\{0,1\}\)$/\1$NEW_VERSION\2/"
-
-    # This regex could be stricter. For example, it doesn't check that in the semver version there are no leading zeros
-    # if the semver number has more than one digit (e.g. v01.1.1 would pass). But right now it's enough and it's much
-    # more readable this way than in "stricter" formats.
-    CURRENT_VERSION=$(gsed -n $GET_IMG_FIELD_REGEXP "deploy/a8s/$IMG.yaml")
-    # This check might misbehave if one of the semver versions has a version number with leading zeros and more than one
-    # digit. But that should never happen since we control those version numbers and there's no reason why we should 
-    # end up having leading zeros in version numbers with more than one digit.
-    if [[ "$NEW_VERSION" > "$CURRENT_VERSION" ]]
+    # Set variables that depend on the specific image that we're considering
+    if [[ "$IMG" == "fluentd" ]]
     then
-        gsed -i $UPDATE_IMG_FIELD_REGEXP "deploy/a8s/$IMG.yaml"
-        # TODO: Uncomment before pushing real version.
-        # git add "deploy/a8s/$IMG.yaml"
-        # git commit -m "Bump $IMG to $NEW_VERSION"
+        echo "fluentd"
+    elif [[ "$IMG" == "opensearch-dashboards" ]]
+    then
+        echo "opensearch-dashboards"
     else
-        echo "$IMG current version is $CURRENT_VERSION, most recent version found is $NEW_VERSION, no update needed"
+        update_core_component_img_and_commit $IMG $NEW_VERSION
     fi
 done
 
