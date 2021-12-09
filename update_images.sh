a60bf65b02f82cb80bdbb77e19c4c1f8421f211d
Matteo Olivi <molivi@anynines.com>
2021-12-09 10:25:45 +0100

    Add some comments on the fragilities that are currently there

--- update_images.sh
+++ update_images.sh
@@ -7,12 +7,15 @@ IMGS="postgresql-operator backup-manager service-binding-controller"
 for IMG in $IMGS
 do
     # TODO: Switch to "sed" when copying inside github action
-    # This regex could be stricter, but right now it's enough and it's much more readable this way
-    # than in "stricter" versions.
+    # This regex could be stricter. For example, it doesn't check that in the semver version there are no leading zeros
+    # if the semver number has more than one digit (e.g. v01.1.1 would pass). But right now it's enough and it's much
+    # more readable this way than in "stricter" formats.
     IMG_FIELD_REGEXP="s/^[[:space:]]\{1,\}image:[[:space:]].\{1,\}\/$IMG:v\([[:digit:]]\{1,\}.[[:digit:]]\{1,\}.[[:digit:]]\{1,\}\)\"\{0,1\}/\1/p"
     CURRENT_VERSION=$(gsed -n $IMG_FIELD_REGEXP "deploy/a8s/$IMG.yaml")
     NEW_VERSION="11.53.22"
-    # TODO: Add comment on fragility of this check.
+    # This check might misbehave if one of the semver versions has a version number with leading zeros and more than one
+    # digit. But that should never happen since we control those version numbers and there's no reason why we should 
+    # end up having leading zeros in version numbers with more than one digit.
     if [[ "$NEW_VERSION" > "$CURRENT_VERSION" ]]
     then
         UPDATE_IMG_FIELD_REGEXP="s/^\([[:space:]]\{1,\}image:[[:space:]].\{1,\}\/$IMG:v\)[[:digit:]]\{1,\}.[[:digit:]]\{1,\}.[[:digit:]]\{1,\}\(\"\{0,1\}\)/\1$NEW_VERSION\2/"
