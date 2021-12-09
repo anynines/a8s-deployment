14865d9c733d9ef76fbd886a03d8867b88d5c185
Matteo Olivi <molivi@anynines.com>
2021-12-09 15:11:01 +0100

    Modify regexp to prepare sharing

--- update_images.sh
+++ update_images.sh
@@ -13,20 +13,22 @@ do
     IMG=$(echo $VERSIONED_IMG | cut -d ':' -f 1)
     NEW_VERSION=$(echo $VERSIONED_IMG | cut -d ':' -f 2)
 
+    GET_IMG_FIELD_REGEXP="s/^[[:space:]]\{1,\}image:[[:space:]].\{1,\}\/$IMG:\(v[\.[:digit:]]\{1,\}\)\"\{0,1\}$/\1/p"
+    UPDATE_IMG_FIELD_REGEXP="s/^\([[:space:]]\{1,\}image:[[:space:]].\{1,\}\/$IMG:\)v[\.[:digit:]]\{1,\}\(\"\{0,1\}\)$/\1$NEW_VERSION\2/p"
+
     # This regex could be stricter. For example, it doesn't check that in the semver version there are no leading zeros
     # if the semver number has more than one digit (e.g. v01.1.1 would pass). But right now it's enough and it's much
     # more readable this way than in "stricter" formats.
-    IMG_FIELD_REGEXP="s/^[[:space:]]\{1,\}image:[[:space:]].\{1,\}\/$IMG:\(v[[:digit:]]\{1,\}.[[:digit:]]\{1,\}.[[:digit:]]\{1,\}\)\"\{0,1\}/\1/p"
-    CURRENT_VERSION=$(gsed -n $IMG_FIELD_REGEXP "deploy/a8s/$IMG.yaml")
+    CURRENT_VERSION=$(gsed -n $GET_IMG_FIELD_REGEXP "deploy/a8s/$IMG.yaml")
     # This check might misbehave if one of the semver versions has a version number with leading zeros and more than one
     # digit. But that should never happen since we control those version numbers and there's no reason why we should 
     # end up having leading zeros in version numbers with more than one digit.
     if [[ "$NEW_VERSION" > "$CURRENT_VERSION" ]]
     then
-        UPDATE_IMG_FIELD_REGEXP="s/^\([[:space:]]\{1,\}image:[[:space:]].\{1,\}\/$IMG:\)v[[:digit:]]\{1,\}.[[:digit:]]\{1,\}.[[:digit:]]\{1,\}\(\"\{0,1\}\)/\1$NEW_VERSION\2/"
         gsed -i $UPDATE_IMG_FIELD_REGEXP "deploy/a8s/$IMG.yaml"
-        git add "deploy/a8s/$IMG.yaml"
-        git commit -m "Bump $IMG to $NEW_VERSION"
+        # TODO: Uncomment before pushing real version.
+        # git add "deploy/a8s/$IMG.yaml"
+        # git commit -m "Bump $IMG to $NEW_VERSION"
     else
         echo "$IMG current version is $CURRENT_VERSION, most recent version found is $NEW_VERSION, no update needed"
     fi
