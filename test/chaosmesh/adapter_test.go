5b64b4bac3450345be3baab454cfd9e86ea1a3b8
Lenny Händler <lhaendler@anynines.com>
2022-08-12 16:33:23 +0200

    wip

--- test/chaosmesh/adapter_test.go
+++ test/chaosmesh/adapter_test.go
@@ -1,1 +1,14 @@
 package chaosmesh_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/anynines/a8s-deployment/test/chaosmesh"
+)
+
+func TestNilObj(t *testing.T) {
+	a := chaosmesh.FaultInjector{}
+
+	a.IsolatePrimary(context.TODO(), nil)
+}
