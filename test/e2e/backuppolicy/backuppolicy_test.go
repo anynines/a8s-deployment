bbfcbbb37abae5831bc9ff89c839253d480595a9
Rabieh Fashwall <rabiehfashwall@Rabiehs-MacBook-Pro.local>
2024-07-04 13:11:00 +0200

    add BackupPolicy Documentation and e2e tests - add a8s-backupPolicy manifests

---
+++ test/e2e/backuppolicy/backuppolicy_test.go
@@ -0,0 +1,204 @@
+package backuppolicy
+
+import (
+	"fmt"
+	"time"
+
+	backupv1beta3 "github.com/anynines/a8s-backup-manager/api/v1beta3"
+	"github.com/anynines/a8s-deployment/test/framework"
+	policy "github.com/anynines/a8s-deployment/test/framework/backuppolicy"
+	"github.com/anynines/a8s-deployment/test/framework/dsi"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	replicas            = 1
+	suffixLength        = 5
+	pollingPeriod       = 1 * time.Second
+	asyncOpsTimeoutMins = time.Minute * 2
+)
+
+var (
+	backupPolicy *backupv1beta3.BackupPolicy
+	instance     dsi.Object
+)
+
+var _ = Describe("BackupPolicy", func() {
+	BeforeEach(func() {
+		// Create Dataservice instance and wait for instance readiness
+		instance, err = dsi.New(
+			dataservice,
+			testingNamespace,
+			framework.GenerateName(instanceNamePrefix, GinkgoParallelProcess(), suffixLength),
+			replicas,
+		)
+		Expect(err).To(BeNil(), "failed to generate new DSI resource")
+
+		Expect(k8sClient.Create(ctx, instance.GetClientObject())).
+			To(Succeed(), fmt.Sprintf("failed to create instance %s/%s",
+				instance.GetNamespace(), instance.GetName()))
+		dsi.WaitForReadiness(ctx, instance.GetClientObject(), k8sClient)
+	})
+
+	AfterEach(func() {
+
+		Expect(k8sClient.Delete(ctx, backupPolicy)).To(Succeed(),
+			fmt.Sprintf("failed to delete backup policy %s/%s",
+				backupPolicy.GetNamespace(), backupPolicy.GetName()))
+		Expect(k8sClient.Delete(ctx, instance.GetClientObject())).To(Succeed(),
+			fmt.Sprintf("failed to delete instance %s/%s",
+				instance.GetNamespace(), instance.GetName()))
+		Expect(policy.CleanupBackups(ctx, backupPolicy, k8sClient)).To(Succeed(), "failed to delete backups")
+		dsi.WaitForDeletion(ctx, instance.GetClientObject(), k8sClient)
+	})
+
+	Context("Backup Policy", func() {
+		It("Should Create BackupPolicy", func() {
+			By("Create a policy", func() {
+				backupPolicy = policy.New(
+					policy.SetNamespacedName(testingNamespace),
+					policy.SetEnabled(true),
+					policy.SetInstanceRef(instance),
+					policy.ScheduleConfiguration(backupv1beta3.ScheduleConfiguration{
+						Type:     backupv1beta3.ScheduleTypeCron,
+						TimeZone: "UTC",
+						Cron: backupv1beta3.Cron{
+							Expression: "2 2 * * *",
+						},
+					}))
+				Expect(k8sClient.Create(ctx, backupPolicy)).To(Succeed(),
+					"failed to create backup policy")
+
+				EventuallyWithOffset(1, func() bool {
+					createdBuckup := &backupv1beta3.BackupPolicy{}
+					if err = k8sClient.Get(ctx, types.NamespacedName{
+						Name:      backupPolicy.GetName(),
+						Namespace: backupPolicy.GetNamespace(),
+					}, createdBuckup); err != nil {
+						return false
+					}
+
+					return createdBuckup.Status.BackupsCount > 0
+
+				}, asyncOpsTimeoutMins, pollingPeriod).Should(BeTrue(),
+					fmt.Sprintf("timeout reached waiting for fetching backupPolicy %s/%s: %s",
+						backupPolicy.GetNamespace(),
+						backupPolicy.GetName(),
+						err,
+					),
+				)
+			})
+		})
+
+		It("Should Create BackupPolicy", func() {
+			By("Create Buckups based on Schedule Config", func() {
+				backupPolicy = policy.New(
+					policy.SetNamespacedName(testingNamespace),
+					policy.SetEnabled(true),
+					policy.SetInstanceRef(instance),
+					policy.ScheduleConfiguration(backupv1beta3.ScheduleConfiguration{
+						Type:     backupv1beta3.ScheduleTypeCron,
+						TimeZone: "UTC",
+						Cron: backupv1beta3.Cron{
+							Expression: "0/1 * * * *",
+						},
+					}))
+				Expect(k8sClient.Create(ctx, backupPolicy)).To(Succeed(),
+					"failed to create backup policy")
+
+				backups := &backupv1beta3.BackupList{}
+
+				EventuallyWithOffset(1, func() bool {
+					createdBuckup := &backupv1beta3.BackupPolicy{}
+					if err = k8sClient.Get(ctx, types.NamespacedName{
+						Name:      backupPolicy.GetName(),
+						Namespace: backupPolicy.GetNamespace(),
+					}, createdBuckup); err != nil {
+						return false
+					}
+					lisOptions := []client.ListOption{
+						client.InNamespace(backupPolicy.Namespace),
+						client.MatchingLabels{backupv1beta3.BackupPolicyNameLabelKey: backupPolicy.Name},
+					}
+
+					if err = k8sClient.List(ctx, backups, lisOptions...); err != nil {
+						return false
+					}
+
+					if len(backups.Items) >= 3 {
+						return true
+					}
+					return false
+
+				}, asyncOpsTimeoutMins, pollingPeriod).Should(BeTrue(),
+					fmt.Sprintf("timeout reached waiting for fetching backupPolicy %s/%s: %s",
+						backupPolicy.GetNamespace(),
+						backupPolicy.GetName(),
+						err,
+					),
+				)
+			})
+		})
+	})
+
+	Context("Retention Configuration", func() {
+		It("Should retain backups based on count", func() {
+			By("Create a policy", func() {
+				backupPolicy = policy.New(
+					policy.SetNamespacedName(testingNamespace),
+					policy.SetEnabled(true),
+					policy.ScheduleConfiguration(backupv1beta3.ScheduleConfiguration{
+						Type:     backupv1beta3.ScheduleTypeCron,
+						TimeZone: "UTC",
+						Cron: backupv1beta3.Cron{
+							Expression: "*/1 * * * *",
+						},
+					}),
+					policy.RetentionConfiguration(&backupv1beta3.RetentionConfiguration{
+						Count: ptr.To(2),
+					}))
+				Expect(k8sClient.Create(ctx, backupPolicy)).To(Succeed(),
+					"failed to create backup policy")
+			})
+
+			By("Create existing backups", func() {
+				for _, backup := range policy.MockExistingBackups(backupPolicy, instance) {
+					Expect(k8sClient.Create(ctx, &backup)).To(Succeed(),
+						"failed to create existing backups")
+				}
+			})
+
+			By("deleting backups over the requested count", func() {
+				var err error
+				backups := &backupv1beta3.BackupList{}
+				EventuallyWithOffset(1, func() bool {
+					lisOptions := []client.ListOption{
+						client.InNamespace(backupPolicy.Namespace),
+						client.MatchingLabels{backupv1beta3.BackupPolicyNameLabelKey: backupPolicy.Name},
+					}
+
+					if err = k8sClient.List(ctx, backups, lisOptions...); err != nil {
+						return false
+					}
+
+					if len(backups.Items) == 2 {
+						return true
+					}
+					return false
+				}, asyncOpsTimeoutMins, pollingPeriod).Should(BeTrue(),
+					fmt.Sprintf("timeout reached waiting for correct backups count %s/%s: %s",
+						backupPolicy.GetNamespace(),
+						backupPolicy.GetName(),
+						err,
+					),
+				)
+				Expect(backups.Items).To(HaveLen(2), "backups cleanup is unsuccessful")
+			})
+		})
+	})
+
+})
---
+++ test/framework/backuppolicy/backuppolicy.go
@@ -0,0 +1,153 @@
+package backuppolicy
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/anynines/a8s-backup-manager/api/v1beta3"
+	"github.com/anynines/a8s-deployment/test/framework"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	runtimeClient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	// asyncOpsTimeoutMins is the amount of minutes after which assertions fail if the condition
+	// they check has not become true. Needed because some conditions might become true only
+	// after some time, so we need to check them asynchronously.
+	// TODO: Make asyncOpsTimeoutMins an invocation parameter.
+	asyncOpsTimeoutMins = time.Minute * 2
+	suffixLength        = 6
+	pollingPeriod       = 1 * time.Second
+)
+
+// Option represents a functional option for backup objects. To learn what a functional option is,
+// read here: https://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis
+type Option func(*v1beta3.BackupPolicy)
+
+func SetNamespacedName(namespace string) Option {
+	return func(p *v1beta3.BackupPolicy) {
+		p.Name = framework.UniqueName("backup-policy", suffixLength)
+		p.Namespace = namespace
+	}
+}
+
+func SetInstanceRef(dsi runtimeClient.Object) Option {
+	return func(p *v1beta3.BackupPolicy) {
+		p.Spec.ServiceInstance.APIGroup = dsi.GetObjectKind().GroupVersionKind().Group
+		p.Spec.ServiceInstance.Kind = dsi.GetObjectKind().GroupVersionKind().Kind
+		p.Spec.ServiceInstance.Name = dsi.GetName()
+	}
+}
+
+func SetEnabled(enabled bool) Option {
+	return func(p *v1beta3.BackupPolicy) {
+		p.Spec.Enabled = enabled
+	}
+}
+
+func ScheduleConfiguration(scheduleConfig v1beta3.ScheduleConfiguration) Option {
+	return func(p *v1beta3.BackupPolicy) {
+		p.Spec.ScheduleConfig = scheduleConfig
+	}
+}
+
+func RetentionConfiguration(retentionConfig *v1beta3.RetentionConfiguration) Option {
+	return func(p *v1beta3.BackupPolicy) {
+		p.Spec.RetentionConfig = retentionConfig
+	}
+}
+
+func New(opts ...Option) *v1beta3.BackupPolicy {
+	p := &v1beta3.BackupPolicy{}
+	for _, opt := range opts {
+		opt(p)
+	}
+	return p
+}
+
+func MockExistingBackups(p *v1beta3.BackupPolicy, dsi runtimeClient.Object) []v1beta3.Backup {
+	existingBackups := []v1beta3.Backup{
+		{
+			ObjectMeta: v1.ObjectMeta{
+				Name:              framework.UniqueName("backup", suffixLength),
+				Namespace:         p.Namespace,
+				CreationTimestamp: v1.Time{Time: mustParseTime("2024-07-03T00:00:00Z")},
+				Labels: map[string]string{
+					v1beta3.BackupPolicyNameLabelKey: p.Name,
+				},
+			},
+			Spec: v1beta3.BackupSpec{
+				ServiceInstance: p.Spec.ServiceInstance,
+			},
+		},
+		{
+			ObjectMeta: v1.ObjectMeta{
+				Name:              framework.UniqueName("backup", suffixLength),
+				Namespace:         p.Namespace,
+				CreationTimestamp: v1.Time{Time: mustParseTime("2024-07-02T00:00:00Z")},
+				Labels: map[string]string{
+					v1beta3.BackupPolicyNameLabelKey: p.Name,
+				},
+			},
+			Spec: v1beta3.BackupSpec{
+				ServiceInstance: p.Spec.ServiceInstance,
+			},
+		},
+		{
+			ObjectMeta: v1.ObjectMeta{
+				Name:              framework.UniqueName("backup", suffixLength),
+				Namespace:         p.Namespace,
+				CreationTimestamp: v1.Time{Time: mustParseTime("2024-07-01T00:00:00Z")},
+				Labels: map[string]string{
+					v1beta3.BackupPolicyNameLabelKey: p.Name,
+				},
+			},
+			Spec: v1beta3.BackupSpec{
+				ServiceInstance: p.Spec.ServiceInstance,
+			},
+		},
+	}
+	return existingBackups
+}
+
+func CreateBackup(ctx context.Context, k8sClient runtimeClient.Client, namespace, creationTime string) {
+	backup := &v1beta3.Backup{
+		ObjectMeta: v1.ObjectMeta{
+			Name:              framework.UniqueName("backup", suffixLength),
+			Namespace:         namespace,
+			CreationTimestamp: v1.Time{Time: mustParseTime(creationTime)},
+		},
+	}
+	k8sClient.Create(ctx, backup)
+}
+
+func mustParseTime(timeStr string) time.Time {
+	parsedTime, err := time.Parse(time.RFC3339, timeStr)
+	if err != nil {
+		panic(fmt.Errorf("error parsing time %w", err))
+	}
+	return parsedTime
+}
+
+func CleanupBackups(ctx context.Context, p *v1beta3.BackupPolicy, c runtimeClient.Client) error {
+	var err error
+	backups := &v1beta3.BackupList{}
+	lisOptions := []client.ListOption{
+		client.InNamespace(p.Namespace),
+		client.MatchingLabels{v1beta3.BackupPolicyNameLabelKey: p.Name},
+	}
+
+	err = c.List(ctx, backups, lisOptions...)
+	if err != nil {
+		return err
+	}
+	for _, backup := range backups.Items {
+		err = c.Delete(ctx, &backup)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
